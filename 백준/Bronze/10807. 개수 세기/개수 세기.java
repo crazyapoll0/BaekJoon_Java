f757c68c8bb9c0ae3dd18491e4379507a1155eb3
JISU KANG <162910117+chasyu0@users.noreply.github.com>
2025-11-10 22:09:32 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 12968 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        
+        for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+        } 
+        
+        int v = sc.nextInt();
+        int sum = 0;
+        
+        for (int i=0; i<N; i++) {
+        	if (arr[i] == v) {
+        		sum++;
+        	}
+        }
+        
+        System.out.println(sum);
+		sc.close();
+	}
+}
