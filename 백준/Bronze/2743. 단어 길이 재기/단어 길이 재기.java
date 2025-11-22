77f454fb502d6430348168740dea264cd2f0afa3
JISU KANG <162910117+chasyu0@users.noreply.github.com>
2025-11-22 22:05:24 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 84 ms, Memory: 12768 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String Alphabet = sc.nextLine();
+        
+        int StringCount = Alphabet.length();
+        
+        System.out.println(StringCount);
+        sc.close();
+    }
+}
+	
